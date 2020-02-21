cff53fcb687398fc763171457b9731085c5b1340
Blake Herrera <blake@blakeherrera.com>
2020-02-20 19:33:59 -0600

    added video background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Countdown from "./Components/Countdown/Countdown.js"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Countdown from "./Components/Countdown/Countdown.js";
 function App() {
   return (
-    <div className="App">
-      <Countdown />
+    <div
+      className="App"
+      style={{
+        overflow: "hidden"
+      }}
+    >
+      <center>
+        <div>
+          <video
+            autoPlay
+            muted
+            loop
+            style={{
+              position: "fixed",
+              left: 0,
+              top: 0,
+              minWidth: "100%",
+              minHeight: "100%"
+            }}
+          >
+            <source
+              src={require("./Assets/americanFlag.mp4")}
+              type="video/mp4"
+            />
+          </video>
+        </div>
+        <br />
+        <div
+          style={{
+            position: "absolute",
+            borderRadius: 25,
+            left: "15%",
+            top: "20%",
+            backgroundColor: "#ffffff",
+            width: "70%",
+            height: "20%",
+            opacity: 0.9
+          }}
+        >
+          <Countdown />
+        </div>
+      </center>
     </div>
   );
 }
